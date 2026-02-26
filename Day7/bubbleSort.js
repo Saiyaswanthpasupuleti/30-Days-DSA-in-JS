a129edcde355faa9c738c8240530f93e2eb46965
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-02-26 16:31:01 +0530

    day7

--- Day7/bubbleSort.js
+++ Day7/bubbleSort.js
@@ -1,12 +1,16 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
+    let swaaped = false;
     for (let j = 0; j < arr.length; j++) {
       console.log(arr[i], arr[j]);
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swaaped = true;
       }
     }
+    if (!swaaped) break;
   }
   return arr;
 }
 console.log(bubbleSort([5, 4, 3, 2, 1]));
+// This has O(n2) timeComplexity

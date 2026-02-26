d060e93150860775c27665ba525f8078960f52e9
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-02-26 16:10:08 +0530

    day7

--- Day7/bubbleSort.js
+++ Day7/bubbleSort.js
@@ -1,18 +1,9 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    console.log("Value of I", i);
-    for (let j = i + 1; j < arr.length - 1; j++) {
-      console.log("Value of J", j);
+    for (let j = 0; j < arr.length; j++) {
+      console.log(arr[i], arr[j]);
       if (arr[j] > arr[j + 1]) {
-        // a=b
-        // b=c
-        // c=a
-        // This is Method 1 by using Temp
-        // var temp=arr[i]
-        // arr[i]=arr[j]
-        // arr[j]=temp
-        // This is the Second Way
-        [arr[i], arr[j + 1]] = [arr[j + 1], arr[i]];
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }

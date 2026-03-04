dcbd0d2376beaf371b6d838218f2b36826ea7bb9
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-03-04 16:29:42 +0530

    return index

--- Day4/slidingWindow.js
+++ Day4/slidingWindow.js
@@ -11,4 +11,30 @@ function slidingWindiow(arr, num) {
   }
   return maxSum;
 }
-console.log(slidingWindiow([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+// console.log(slidingWindiow([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+function primeNumber(num) {
+  for (let i = 2; i <= num; i++) {
+    var prime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        prime = false;
+        break;
+      }
+    }
+    if (prime) {
+      console.log(i);
+    }
+  }
+}
+// console.log(primeNumber(10));
+
+function returnIndex(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+console.log(returnIndex([1, 2, 3, 4, 5, 6], 4));
